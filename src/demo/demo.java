3770f4bb3930ed1aa585a84b5ca51059821be0b6
sari <sari@DESKTOP-U7TGTT1.mshome.net>
2025-02-12 10:47:38 +0100

    agrupando los ultimos 4 commits de ramaTrabajo añadiendo linea 3 en rebase interactivo linea 4 en rebase interactivo linea 5 en rebase interactivo linea 6 en rebase interactivo

--- src/demo/demo.java
+++ src/demo/demo.java
@@ -12,6 +12,12 @@ public class demo {
 		System.out.println("linea 2 de rama master");
 
 		System.out.println("linea 2 de rama trabajo");
+		
+		System.out.println("linea 3 rebase interactivo");
+		System.out.println("linea 4 rebase interactivo");
+		System.out.println("linea 5 rebase interactivo");
+		System.out.println("linea 6 rebase interactivo");
+		
 
 
 	}
